8e115a289624f1e5ccba8f990b1462fa627b0751
Stratex <aqja88@gmail.com>
2015-10-15 21:19:46 +1300

    Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Simple_Filemanager
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listDir = new System.Windows.Forms.ListView();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.cmdGo = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.comboDrive = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // listDir
+            // 
+            this.listDir.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listDir.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listDir.Location = new System.Drawing.Point(12, 38);
+            this.listDir.Name = "listDir";
+            this.listDir.Size = new System.Drawing.Size(549, 331);
+            this.listDir.TabIndex = 0;
+            this.listDir.UseCompatibleStateImageBehavior = false;
+            this.listDir.DoubleClick += new System.EventHandler(this.listDir_DoubleClick);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAddress.Location = new System.Drawing.Point(68, 12);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(439, 20);
+            this.txtAddress.TabIndex = 1;
+            // 
+            // cmdGo
+            // 
+            this.cmdGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdGo.Location = new System.Drawing.Point(513, 12);
+            this.cmdGo.Name = "cmdGo";
+            this.cmdGo.Size = new System.Drawing.Size(48, 23);
+            this.cmdGo.TabIndex = 2;
+            this.cmdGo.Text = "Go";
+            this.cmdGo.UseVisualStyleBackColor = true;
+            this.cmdGo.Click += new System.EventHandler(this.cmdGo_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            // 
+            // comboDrive
+            // 
+            this.comboDrive.FormattingEnabled = true;
+            this.comboDrive.Location = new System.Drawing.Point(12, 11);
+            this.comboDrive.Name = "comboDrive";
+            this.comboDrive.Size = new System.Drawing.Size(50, 21);
+            this.comboDrive.TabIndex = 3;
+            this.comboDrive.SelectedIndexChanged += new System.EventHandler(this.comboDrive_SelectedIndexChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 381);
+            this.Controls.Add(this.comboDrive);
+            this.Controls.Add(this.cmdGo);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.listDir);
+            this.Name = "frmMain";
+            this.Text = "Simple File Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listDir;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Button cmdGo;
+        private System.Windows.Forms.ComboBox comboDrive;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Filemanager
+{
+    public partial class frmMain : Form
+    {
+        string dir = @"C:\";
+        public frmMain()
+        {
+            InitializeComponent();
+            listDir.View = View.Details;
+            listDir.Columns.Add("Date Modified");
+            listDir.Columns.Add("Filesize");
+            foreach (string item in GetLogicalDrives())
+            {
+                comboDrive.Items.Add(item);
+            }
+            comboDrive.SelectedItem = dir;
+
+            DirectoryInfo di = new DirectoryInfo(dir);
+            FullDirList(di);
+            foreach (DirectoryInfo folder in folders)
+                listDir.Items.Add(new ListViewItem(new string[] { folder.Name, folder.LastWriteTimeUtc.ToShortDateString(), "Folder" }));
+            foreach (FileInfo file in files)
+                listDir.Items.Add(new ListViewItem(new string[] { file.Name, file.LastWriteTimeUtc.ToShortDateString(), (file.Length / 1000).ToString() + "kb" }));
+            listDir.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+        }
+
+        static List<FileInfo> files = new List<FileInfo>();  // List that will hold the files and subfiles in path
+        static List<DirectoryInfo> folders = new List<DirectoryInfo>(); // List that hold direcotries that cannot be accessed
+        static void FullDirList(DirectoryInfo dir)
+        {
+            files.Clear();
+            folders.Clear();
+            try
+            {
+                foreach (FileInfo f in dir.GetFiles())
+                {
+                    files.Add(f);
+                }
+            }
+            catch
+            {
+                return;
+            }
+
+            foreach (DirectoryInfo d in dir.GetDirectories())
+            {
+                folders.Add(d);
+            }
+
+        }
+        public List<string> GetLogicalDrives()
+        {
+            List<string> DriveList = new List<string>();
+            try
+            {
+                string[] drives = Directory.GetLogicalDrives();
+
+                foreach (string drive in drives)
+                {
+                    DriveList.Add(drive);
+                }
+            }
+            catch
+            { }
+            return DriveList;
+        }
+
+        private void listDir_DoubleClick(object sender, EventArgs e)
+        {
+            FileAttributes attr = File.GetAttributes(comboDrive.Text + txtAddress.Text + listDir.SelectedItems[0].Text);
+            if (attr.HasFlag(FileAttributes.Directory))
+            {
+                txtAddress.Text += listDir.SelectedItems[0].Text + @"\";
+                DirectoryInfo dir = new DirectoryInfo(comboDrive.Text + txtAddress.Text);
+                FullDirList(dir);
+                listDir.Items.Clear();
+                foreach (DirectoryInfo folder in folders)
+                    listDir.Items.Add(new ListViewItem(new string[] { folder.Name, folder.LastWriteTimeUtc.ToShortDateString(), "Folder" }));
+                foreach (FileInfo file in files)
+                    listDir.Items.Add(new ListViewItem(new string[] { file.Name, file.LastWriteTimeUtc.ToShortDateString(), (file.Length / 1000).ToString() + "kb" }));
+                listDir.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            }
+            else
+                Process.Start(comboDrive.Text + txtAddress.Text + listDir.SelectedItems[0].Text);
+            //File.Open(comboDrive.Text + txtAddress.Text + listDir.SelectedItems[0].Text, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+        }
+        
+
+        private void cmdGo_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo dir = new DirectoryInfo(comboDrive.Text + txtAddress.Text);
+            FullDirList(dir);
+            listDir.Items.Clear();
+            foreach (DirectoryInfo folder in folders)
+                listDir.Items.Add(new ListViewItem(new string[] { folder.Name, folder.LastWriteTimeUtc.ToShortDateString(), "Folder"}));
+            foreach (FileInfo file in files)
+                listDir.Items.Add(new ListViewItem(new string[] { file.Name, file.LastWriteTimeUtc.ToShortDateString(), (file.Length / 1000).ToString() + "kb" }));
+            listDir.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+        }
+
+        private void comboDrive_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtAddress.Focus();
+            txtAddress.SelectAll();
+        }
+    }
+}
